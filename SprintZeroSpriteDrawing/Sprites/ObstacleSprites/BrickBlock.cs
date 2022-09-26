35b00a4a71a7c956675035263baae13d033054fb
Zhongyao Wang <wang.12405@buckeyemail.osu.edu>
2022-09-26 14:28:55 -0400

    Updates my spriteFactory and different block states

--- SprintZeroSpriteDrawing/Sprites/ObstacleSprites/BrickBlock.cs
+++ SprintZeroSpriteDrawing/Sprites/ObstacleSprites/BrickBlock.cs
@@ -27,6 +27,7 @@ namespace SprintZeroSpriteDrawing.Sprites.ObstacleSprites
 
         public BrickBlock(Texture2D nSprite, Vector2 nSheetSize, Vector2 nPos)
         {
+            //IsVis = true;
             SubframeLimit = 20;
             AutoFrame = true;
             Sprite = nSprite;
