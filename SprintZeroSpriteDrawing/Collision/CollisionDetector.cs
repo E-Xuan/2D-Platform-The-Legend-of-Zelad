8cf35d3c0d459929cb86e5a32b38b778a8197d6e
Zhongyao Wang <wang.12405@buckeyemail.osu.edu>
2022-10-02 16:30:46 -0400

    Update a potential collision dector. Could be used in future for detecting collision.

---
+++ SprintZeroSpriteDrawing/Collision/CollisionDetector.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using SprintZeroSpriteDrawing.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprintZeroSpriteDrawing.Collision
+{
+    public class CollisionDetector
+    {
+        int velocity = 5;
+        public bool IsTouchingLeft(Rectangle firstObject, Rectangle secondObject)
+        {
+            
+            return firstObject.Right + velocity > secondObject.Left && 
+                firstObject.Left < secondObject.Left &&
+                firstObject.Bottom > secondObject.Top &&
+                firstObject.Top < secondObject.Bottom;
+        }
+        public bool IsTouchingRight(Rectangle firstObject, Rectangle secondObject)
+        {
+            return firstObject.Left + velocity > secondObject.Right &&
+                firstObject.Right < secondObject.Right &&
+                firstObject.Bottom > secondObject.Top &&
+                firstObject.Top < secondObject.Bottom;
+        }
+        public bool ISTouchingTop(Rectangle firstObject, Rectangle secondObject)
+        {
+            return firstObject.Bottom + velocity > secondObject.Top &&
+                firstObject.Top < secondObject.Top &&
+                firstObject.Right > secondObject.Left &&
+                firstObject.Left < secondObject.Right;
+        }
+        public bool ISTouchingDown(Rectangle firstObject, Rectangle secondObject)
+        {
+            return firstObject.Top + velocity > secondObject.Bottom &&
+                firstObject.Bottom < secondObject.Bottom &&
+                firstObject.Right > secondObject.Left &&
+                firstObject.Left < secondObject.Right;
+        }
+    }
+}
