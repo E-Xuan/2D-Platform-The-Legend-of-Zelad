a4dfa358c35b58ff987a7f7b939130b7e106fd90
Zhongyao Wang <wang.12405@buckeyemail.osu.edu>
2022-10-09 23:28:52 -0400

    simple folder for enemy collision responses

---
+++ SprintZeroSpriteDrawing/Collision/EnemyCollision/GreenKoopa.cs
@@ -0,0 +1,16 @@
+﻿using SprintZeroSpriteDrawing.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprintZeroSpriteDrawing.Collision.EnemyCollision
+{
+    internal class GreenKoopa : ICommand
+    {
+        public GreenKoopa(object nRef) : base(nRef)
+        {
+        }
+    }
+}
