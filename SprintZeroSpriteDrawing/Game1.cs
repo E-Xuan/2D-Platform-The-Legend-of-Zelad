03f54c6b41141636969f87fe1b80565da637de2f
YifeiZhuang <zhuang.256@buckeyemail.osu.edu>
2022-10-11 19:57:56 -0400

    sdf

--- SprintZeroSpriteDrawing/Game1.cs
+++ SprintZeroSpriteDrawing/Game1.cs
@@ -68,6 +68,8 @@ namespace SprintZeroSpriteDrawing
         #endregion
 
         public static Vector2 LEVELSIZE = new Vector2(1920,1080);
+        public static Vector2 WINDOWSIZE = new Vector2(1920, 1080);
+
         public static bool DEBUGBBOX = true;
         public Game1()
         {
