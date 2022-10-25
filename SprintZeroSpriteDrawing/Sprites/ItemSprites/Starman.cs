820595b71c930c65b7094184a3449b6aaad9ffa5
Yixuan Huang <huang.4013@buckeyemail.osu.edu>
2022-10-25 19:31:57 -0400

    Starman moving and Mushroom moving

--- SprintZeroSpriteDrawing/Sprites/ItemSprites/Starman.cs
+++ SprintZeroSpriteDrawing/Sprites/ItemSprites/Starman.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using SprintZeroSpriteDrawing.Interfaces.Entitiy;
+using SprintZeroSpriteDrawing.Collision.CollisionManager;
 
 namespace SprintZeroSpriteDrawing.Sprites.ItemSprites
 {
@@ -10,6 +11,17 @@ namespace SprintZeroSpriteDrawing.Sprites.ItemSprites
         public Starman(Texture2D nSprite, Vector2 nSheetSize, Vector2 nPos) : base(nSprite, nSheetSize, nPos)
         {
             
+        }
+        public override void Update()
+        {
+            base.Update();
+
+            if (State.CurrState == Interfaces.ItemState.State.EMERGING)
+            {
+                Velocity = new Vector2(4, 0);
+                Acceleration = new Vector2(0, (float).065);
+                CollisionManager.getCM().RegMoving(this);
+            }
         }
     }
 }
--- SprintZeroSpriteDrawing/Sprites/ItemSprites/SuperMushroom.cs
+++ SprintZeroSpriteDrawing/Sprites/ItemSprites/SuperMushroom.cs
@@ -11,6 +11,18 @@ namespace SprintZeroSpriteDrawing.Sprites.ItemSprites
         public SuperMushroom(Texture2D nSprite, Vector2 nSheetSize, Vector2 nPos) : base(nSprite, nSheetSize, nPos)
         {
             CollideableType = CType.LEVELUP;
+            
+        }
+        public override void Update()
+        {
+            base.Update();
+            
+            if (State.CurrState == Interfaces.ItemState.State.EMERGING)
+            {
+                Velocity = new Vector2(1, 0);
+                Acceleration = new Vector2(0, (float).065);
+                CollisionManager.getCM().RegMoving(this);
+            }
         }
     }
 }
