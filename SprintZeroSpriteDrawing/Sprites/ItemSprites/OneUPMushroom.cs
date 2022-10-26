786ea0a3851518d762bda438a382992ca305ab28
Phillip White <phillip.white@lakeshore.com>
2022-10-25 22:28:33 -0400

    aofjwuoaefnsdjnf

--- SprintZeroSpriteDrawing/Sprites/ItemSprites/OneUPMushroom.cs
+++ SprintZeroSpriteDrawing/Sprites/ItemSprites/OneUPMushroom.cs
@@ -1,15 +1,33 @@
-﻿using SprintZeroSpriteDrawing.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using SprintZeroSpriteDrawing.Interfaces;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using SprintZeroSpriteDrawing.Collision.CollisionManager;
+using SprintZeroSpriteDrawing.Commands;
 using SprintZeroSpriteDrawing.Interfaces.Entitiy;
 
 namespace SprintZeroSpriteDrawing.Sprites.ItemSprites
 {
     public class OneUPMushroom : Item
-    {    
+    {
+        private bool emerge = false;
         public OneUPMushroom(Texture2D nSprite, Vector2 nSheetSize, Vector2 nPos) : base(nSprite, nSheetSize, nPos)
         {
+            CollisionResponse.Add(new Tuple<ICommand, Direction, CType>(new IntCmd(new KeyValuePair<Action<int>, int>(BounceWalled, 1)), Direction.SIDE, CType.NEUTRAL));
+            CollisionResponse.Add(new Tuple<ICommand, Direction, CType>(new IntCmd(new KeyValuePair<Action<int>, int>(Floored, 1)), Direction.BOTTOM, CType.NEUTRAL));
+        }
+        public override void Update()
+        {
+            base.Update();
 
+            if (State.CurrState == Interfaces.ItemState.State.EMERGING && !emerge)
+            {
+                Velocity = new Vector2(2, 0);
+                Acceleration = new Vector2(0, (float).065);
+                CollisionManager.getCM().RegMoving(this);
+                emerge = true;
+            }
         }
     }
 }
