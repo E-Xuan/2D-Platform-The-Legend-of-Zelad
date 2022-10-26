32aa5cd75452d5700e6052f9a75f4e743ce1b0d5
YifeiZhuang <zhuang.256@buckeyemail.osu.edu>
2022-10-25 22:24:53 -0400

    1231231

--- SprintZeroSpriteDrawing/Interfaces/EnemyState/ShellMoving.cs
+++ SprintZeroSpriteDrawing/Interfaces/EnemyState/ShellMoving.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using SprintZeroSpriteDrawing.Sprites.EnemySprites;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SprintZeroSpriteDrawing.Interfaces.EnemyState
 {
-    internal class ShellMoving
+    public class ShellMoving : IEnemyState
     {
+        public ShellMoving(Enemy nEnemy) : base(nEnemy)
+        {
+            CurrState = State.SHELLMOVING;
+        }
+        public override void Enter()
+        {
+            base.Enter();
+        }
     }
 }
