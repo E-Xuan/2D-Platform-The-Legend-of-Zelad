db82e8ffd919c96e452ac148c19aecf4f2d451da
tates2 <tates.2@osu.edu>
2022-11-06 15:15:15 -0500

    Delagate system

--- SprintZeroSpriteDrawing/Music_SoundEffects/SoundEffectPlayer.cs
+++ SprintZeroSpriteDrawing/Music_SoundEffects/SoundEffectPlayer.cs
@@ -11,6 +11,13 @@ namespace SprintZeroSpriteDrawing.Music_SoundEffects
     public class SoundEffectPlayer
     {
 
+        public event EventHandler<EventArgs> PlaySoundEffect;
+
+        protected virtual void OnSoundEffectPlayed(EventArgs sound)
+        {
+            PlaySoundEffect?.Invoke(this, sound);
+        }
+
         public void LoadSongs(ContentManager content)
         {
              
